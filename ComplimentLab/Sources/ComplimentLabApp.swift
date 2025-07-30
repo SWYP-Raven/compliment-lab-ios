378e8e22cc7c16ad8bf8f472fc81225311836e21
Lee Inho <inhoking01@gmail.com>
2025-07-31 00:16:14 +0900

    feat. init project with tuist

---
+++ ComplimentLab/Sources/ComplimentLabApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct ComplimentLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Project.swift
@@ -0,0 +1,34 @@
+import ProjectDescription
+
+let project = Project(
+    name: "ComplimentLab",
+    targets: [
+        .target(
+            name: "ComplimentLab",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "com.raven.complimentlab",
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchScreen": [
+                        "UIColorName": "",
+                        "UIImageName": "",
+                    ],
+                ]
+            ),
+            sources: ["ComplimentLab/Sources/**"],
+            resources: ["ComplimentLab/Resources/**"],
+            dependencies: []
+        ),
+        .target(
+            name: "ComplimentLabTests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: "dev.tuist.ComplimentLabTests",
+            infoPlist: .default,
+            sources: ["ComplimentLab/Tests/**"],
+            resources: [],
+            dependencies: [.target(name: "ComplimentLab")]
+        ),
+    ]
+)
