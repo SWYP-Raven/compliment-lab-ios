e639a57d0c372eb0eee55a11b8e203e10f09ac7d
Lee Inho <inhoking01@gmail.com>
2025-08-17 22:15:22 +0900

    feat. 색상 asset 추가

---
+++ ComplimentLab/Sources/Common/Extensions/Color+Extension.swift
@@ -0,0 +1,40 @@
+//
+//  Color+Extension.swift
+//  ComplimentLab
+//
+//  Created by 이인호 on 8/17/25.
+//
+
+import SwiftUI
+
+extension Color {
+    static let gray1 = Color("gray1")
+    static let gray2 = Color("gray2")
+    static let gray3 = Color("gray3")
+    static let gray4 = Color("gray4")
+    static let gray5 = Color("gray5")
+    static let gray6 = Color("gray6")
+    static let gray7 = Color("gray7")
+    static let gray8 = Color("gray8")
+    static let gray9 = Color("gray9")
+    
+    static let pink1 = Color("pink1")
+    static let pink2 = Color("pink2")
+    static let pink3 = Color("pink3")
+    static let pink4 = Color("pink4")
+    static let pink5 = Color("pink5")
+    
+    static let blue1 = Color("blue1")
+    static let blue2 = Color("blue2")
+    static let blue3 = Color("blue3")
+    static let blue4 = Color("blue4")
+    
+    static let violet1 = Color("violet1")
+    static let violet2 = Color("violet2")
+    
+    static let yellow1 = Color("yellow1")
+    static let yellow2 = Color("yellow2")
+    
+    static let green1 = Color("green1")
+    static let green2 = Color("green2")
+}
