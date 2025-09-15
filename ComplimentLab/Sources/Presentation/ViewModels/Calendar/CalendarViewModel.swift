6c510a65994b3c01589c55c7a88f719569103329
Inho Lee <harrylee0116@gmail.com>
2025-09-15 13:37:31 +0900

    chore. 파일 분리

--- ComplimentLab/Sources/Presentation/ViewModels/Calendar/CalendarViewModel.swift
+++ ComplimentLab/Sources/Presentation/ViewModels/Calendar/CalendarViewModel.swift
@@ -7,24 +7,6 @@
 
 import Foundation
 
-/// - past: 오늘 이전 날짜
-/// - today: 오늘 날짜
-/// - future: 오늘 이후 날짜
-/// - outsideMonth: 해당 월에 속하지 않음
-enum DateState {
-    case past
-    case today
-    case future
-    case outsideMonth
-}
-
-enum CalendarMode: String, CaseIterable, CustomStringConvertible {
-    case week = "주간"
-    case month = "월간"
-    
-    var description: String { rawValue }
-}
-
 @MainActor
 final class CalendarViewModel: ObservableObject {
     let calendar = Calendar.current
--- ComplimentLab/Sources/Presentation/ViewModels/Calendar/DateState.swift
+++ ComplimentLab/Sources/Presentation/ViewModels/Calendar/DateState.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+/// - past: 오늘 이전 날짜
+/// - today: 오늘 날짜
+/// - future: 오늘 이후 날짜
+/// - outsideMonth: 해당 월에 속하지 않음
+enum DateState {
+    case past
+    case today
+    case future
+    case outsideMonth
+}
+
+enum CalendarMode: String, CaseIterable, CustomStringConvertible {
+    case week = "주간"
+    case month = "월간"
+    
+    var description: String { rawValue }
+}
